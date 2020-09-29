5930706cd3eb6564709fd0f1acfeb6387d739c39
Maddie Vorhies <Maddie_Vorhies1@baylor.edu>
2020-09-28 23:25:17 -0500

    Lab05

--- Lab05/systemverilog/addsub_test.sv
+++ Lab05/systemverilog/addsub_test.sv
@@ -20,7 +20,39 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module addsub_test(
+module addsub_test();
 
-    );
+   reg [1:0] a_t, b_t;
+   reg mode_t;
+   wire [1:0] sum_t;
+   wire cbout_t;
+   
+   addsub dut (
+     
+     .a(a_t),
+     .b(b_t),
+     .mode(mode_t),
+     .sum(sum_t),
+     .cbout(cbout_t)
+  );
+  
+  initial begin 
+  
+     mode_t = 0; a_t[1] = 0; a_t[0] = 0; b_t[1] = 0; b_t[0] = 1; #10;
+     mode_t = 0; a_t[1] = 0; a_t[0] = 0; b_t[1] = 1; b_t[0] = 0; #10;
+     mode_t = 0; a_t[1] = 0; a_t[0] = 0; b_t[1] = 1; b_t[0] = 1; #10;
+     mode_t = 0; a_t[1] = 0; a_t[0] = 1; b_t[1] = 0; b_t[0] = 1; #10;
+     mode_t = 0; a_t[1] = 1; a_t[0] = 0; b_t[1] = 0; b_t[0] = 1; #10;
+     mode_t = 0; a_t[1] = 1; a_t[0] = 0; b_t[1] = 0; b_t[0] = 0; #10;
+     mode_t = 1; a_t[1] = 0; a_t[0] = 0; b_t[1] = 0; b_t[0] = 1; #10;
+     mode_t = 1; a_t[1] = 0; a_t[0] = 0; b_t[1] = 1; b_t[0] = 0; #10;
+     mode_t = 1; a_t[1] = 0; a_t[0] = 0; b_t[1] = 1; b_t[0] = 1; #10;
+     mode_t = 1; a_t[1] = 0; a_t[0] = 1; b_t[1] = 0; b_t[0] = 1; #10;
+     mode_t = 1; a_t[1] = 1; a_t[0] = 0; b_t[1] = 0; b_t[0] = 1; #10;
+     mode_t = 1; a_t[1] = 1; a_t[0] = 0; b_t[1] = 0; b_t[0] = 0; #10;
+     
+     $finish;
+     
+  end
+  
 endmodule
